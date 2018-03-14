57e8e5495dcaff7b974732d1cf8dda8a96096fd5
J Couderc <32571260+Kryddd@users.noreply.github.com>
2018-03-14 12:33:49 +0100

    Fix clients

--- BRiLaunch clientama/src/client/App.java
+++ BRiLaunch clientama/src/client/App.java
@@ -9,28 +9,26 @@ import java.net.Socket;
 public class App {
 	public final static int PORT_AMA = 2700;
 	public final static String adresse = "localhost";
-	
+
 	private static Socket socket;
 
 	public static void main(String[] args) throws IOException {
-		
-		
-		//new Thread(new Client(adresse, PORT_AMA)).start();
-		
-		
+
+		// new Thread(new Client(adresse, PORT_AMA)).start();
+
 		socket = new Socket(adresse, PORT_AMA);
-		
+
 		BufferedReader sIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		PrintWriter sOut = new PrintWriter(socket.getOutputStream(), true);
-		
+
 		BufferedReader clavier = new BufferedReader(new InputStreamReader(System.in));
-		
-			while (true) {
-				System.out.println(sIn.readLine());
-				sOut.println(clavier.readLine());
-			}
+
+		while (true) {
+			System.out.println(sIn.readLine());
+			sOut.println(clavier.readLine());
+		}
 	}
-	
+
 	protected void finalize() throws IOException {
 		socket.close();
 	}
--- BRiLaunch clientprog/src/client/App.java
+++ BRiLaunch clientprog/src/client/App.java
@@ -1,13 +1,35 @@
 package client;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class App {
 	public final static int PORT_PROG = 2600;
 	public final static String adresse = "localhost";
-	
-	
-	
-	public static void main(String[] args) throws IOException{
-		new Thread(new Client(adresse, PORT_PROG)).start();
+
+	private static Socket socket;
+
+	public static void main(String[] args) throws IOException {
+
+		// new Thread(new Client(adresse, PORT_AMA)).start();
+
+		socket = new Socket(adresse, PORT_PROG);
+
+		BufferedReader sIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter sOut = new PrintWriter(socket.getOutputStream(), true);
+
+		BufferedReader clavier = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			System.out.println(sIn.readLine());
+			sOut.println(clavier.readLine());
+		}
+	}
+
+	protected void finalize() throws IOException {
+		socket.close();
 	}
 }
